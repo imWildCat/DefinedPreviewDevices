512530dc7778a059028e6f72000507b2b6a2869b
WildCat <wildcat.name@gmail.com>
2021-05-16 11:34:48 +0800

    Better namespacing

---
+++ Sources/DefinedPreviewDevices/DefinedPreviewDevices+Device.swift
@@ -0,0 +1,8 @@
+#if canImport(SwiftUI)
+  @available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
+  public extension DefinedPreviewDevices {
+    enum Device: String, CaseIterable {
+      case mac = "Mac"
+    }
+  }
+#endif
--- Sources/DefinedPreviewDevices/DefinedPreviewDevices.swift
+++ Sources/DefinedPreviewDevices/DefinedPreviewDevices.swift
@@ -1,9 +1,7 @@
 #if canImport(SwiftUI)
   @available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
   public enum DefinedPreviewDevices {
-    public enum Device: String, CaseIterable {
-      case mac = "Mac"
-    }
+   
   }
 
 #endif
