98a66e28a58e2ff0c8078853ecded0b32948e37c
WildCat <wildcat.name@gmail.com>
2021-05-16 17:00:07 +0800

    Update

--- Tests/DefinedPreviewDevicesTests/DefinedPreviewDevicesTests.swift
+++ Tests/DefinedPreviewDevicesTests/DefinedPreviewDevicesTests.swift
@@ -1,11 +1,8 @@
-    import XCTest
-    @testable import DefinedPreviewDevices
+@testable import DefinedPreviewDevices
+import XCTest
 
-    final class DefinedPreviewDevicesTests: XCTestCase {
-        func testExample() {
-            // This is an example of a functional test case.
-            // Use XCTAssert and related functions to verify your tests produce the correct
-            // results.
-            // XCTAssertEqual(DefinedPreviewDevices().text, "Hello, World!")
-        }
+final class DefinedPreviewDevicesTests: XCTestCase {
+    func testExample() {
+        XCTAssertEqual(DefinedPreviewDevices.Device.allCases.count, 68 + 2)
     }
+}
