0997a99240031d3b016d6121e012f28394327a75
WildCat <wildcat.name@gmail.com>
2021-05-16 15:00:00 +0800

    Fix name

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "DefinedDefinedPreviewDevices",
+    name: "DefinedPreviewDevices",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "DefinedDefinedPreviewDevices",
+            name: "DefinedPreviewDevices",
             targets: ["DefinedPreviewDevices"]),
     ],
     dependencies: [
